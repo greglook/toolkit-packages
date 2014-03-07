521762aa776f7f00ecebaf8741545f83fb4d277b
Greg Look <greg@greg-look.net>
2014-03-07 15:17:09 -0800

    Add jonase/eastwood plugin to lein.

--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -2,7 +2,8 @@
  :dependencies
  [[org.clojure/tools.trace "0.7.6"]]
  :plugins
- [[lein-cloverage "1.0.2"]
+ [[jonase/eastwood "0.1.0"]
+  [lein-cloverage "1.0.2"]
   [lein-exec "0.3.1"]
   [lein-kibit "0.0.8"]
   [lein-ns-dep-graph "0.1.0-SNAPSHOT"]
