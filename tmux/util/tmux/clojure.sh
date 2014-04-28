4093b44eae0cf0a48296771b0b71879cdbccd9d3
Greg Look <greg@greg-look.net>
2014-04-27 19:22:14 -0700

    Update clojure tmux setup script.

--- tmux/util/tmux/clojure.sh
+++ tmux/util/tmux/clojure.sh
@@ -35,17 +35,18 @@ tmux new-window -t "$SESSION:0" -n 'misc' -c "$HOME" -k
 
 tmux new-window -t "$SESSION:1" -n 'git' -c "$PROJECT_ROOT"
 
-tmux new-window -t "$SESSION:2" -n 'src' -c "$PROJECT_ROOT/src/$NAMESPACE"
-tmux split-window -t "$SESSION:2" -h -l 60 -c "$PROJECT_ROOT"
-tmux send-keys -t "$SESSION:2.1" "lein trampoline repl" # C-m
+tmux new-window -t "$SESSION:2" -n 'repl' -c "$PROJECT_ROOT/src/$NAMESPACE"
+tmux send-keys -t "$SESSION:2" "lein trampoline repl" # C-m
 
-tmux new-window -t "$SESSION:3" -n 'test' -c "$PROJECT_ROOT/test/$NAMESPACE"
-tmux split-window -t "$SESSION:3" -h -c "$PROJECT_ROOT"
-tmux send-keys -t "$SESSION:3.1" "lein test-refresh" # C-m
+tmux new-window -t "$SESSION:3" -n 'src' -c "$PROJECT_ROOT/src/$NAMESPACE"
 
-tmux new-window -t "$SESSION:4" -n 'doc'  -c "$PROJECT_ROOT/doc"
+tmux new-window -t "$SESSION:4" -n 'test' -c "$PROJECT_ROOT/test/$NAMESPACE"
+tmux split-window -t "$SESSION:4" -h -c "$PROJECT_ROOT"
+tmux send-keys -t "$SESSION:4.1" "lein test-refresh" # C-m
+
+tmux new-window -t "$SESSION:5" -n 'doc'  -c "$PROJECT_ROOT/doc"
 
 # Attach to session
-tmux select-window -t "$SESSION:2"
+tmux select-window -t "$SESSION:3"
 tmux select-window -t "$SESSION:1"
 tmux attach -t "$SESSION"
