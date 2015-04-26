0405d5fdf27ca043917e3a9f19dc359daa8ac877
Greg Look <greg@greg-look.net>
2015-04-25 18:16:29 -0700

    Split out whidbey into repl profile.

---
+++ lein/lein/profiles.d/repl.clj
@@ -0,0 +1,8 @@
+{:plugins
+ [[mvxcvi/whidbey "0.6.0"]]
+
+ :whidbey
+ {:width 200
+  :map-delimiter ","
+  :escape-types #{'datomic.db.Db
+                  'datomic.btset.BTSet}}}
--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -1,24 +1,40 @@
-{:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+{:aliases
+ {"doc-lit" ^:displace ["marg" "--dir" "doc/marginalia"]
+  "docs" ["do" ["doc"] ["doc-lit"] ["hiera"]]
+  "tests" ["do" ["check"] ["test"] ["cloverage"]]
+  "slamhound" ["run" "-m" "slam.hound"]}
 
- :dependencies [[clj-stacktrace "0.2.7"]
-                [slamhound "1.5.5"]
-                [org.clojure/tools.trace "0.7.6"]]
+ :plugins
+ [[codox "0.8.10"]
+  [com.jakemccrary/lein-test-refresh "0.5.3"]
+  [jonase/eastwood "0.1.4"]
+  [lein-ancient "0.6.1"]
+  [lein-cljfmt "0.1.4"]
+  [lein-cloverage "1.0.2"]
+  [lein-cprint "1.0.0"]
+  [lein-hiera "0.9.0"]
+  [lein-kibit "0.0.8"]
+  [lein-marginalia "0.8.0"]
+  [lein-vanity "0.2.0"]]
 
- :plugins [[lein-ancient "0.5.5"]
-           [lein-cloverage "1.0.2"]
-           [lein-cprint "1.0.0"]
-           [lein-hiera "0.9.0"]
-           [lein-kibit "0.0.8"]
-           [lein-marginalia "0.8.0"]
-           [lein-vanity "0.2.0"]
-           [com.jakemccrary/lein-test-refresh "0.5.3"]
-           [jonase/eastwood "0.1.4"]]
+ :dependencies
+ [[clj-stacktrace "0.2.7"]
+  [slamhound "1.5.5"]
+  [org.clojure/tools.namespace "0.2.8"]
+  [org.clojure/tools.trace "0.7.8"]]
 
  :injections
  [(let [pct-var (ns-resolve (doto 'clojure.stacktrace require) 'print-cause-trace)
         pst-var (ns-resolve (doto 'clj-stacktrace.repl require) 'pst+)]
     (alter-var-root pct-var (constantly (deref pst-var))))]
 
- :hiera ^:displace
- {:show-external? true
+ :codox
+ {:defaults {:doc/format :markdown}
+  :exclude #{user}
+  :output-dir "doc/api"
+  :src-linenum-anchor-prefix "L"}
+
+ :hiera
+ {:path "doc/ns-hiera.png"
+  :show-external? true
   :ignore-ns #{clojure user}}}
