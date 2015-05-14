9e222359017b8c37a9c6a1136437b60f4f945696
Greg Look <greglook@counsyl.com>
2015-05-14 12:50:24 -0700

    Merge local changes.

--- lein/lein/profiles.d/repl.clj
+++
@@ -1,8 +0,0 @@
-{:plugins
- [[mvxcvi/whidbey "0.6.0"]]
-
- :whidbey
- {:width 200
-  :map-delimiter ","
-  :escape-types #{'datomic.db.Db
-                  'datomic.btset.BTSet}}}
--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -6,16 +6,18 @@
 
  :plugins
  [[codox "0.8.10"]
-  [com.jakemccrary/lein-test-refresh "0.5.3"]
+  [com.jakemccrary/lein-test-refresh "0.5.3" :exclusions [org.clojure/clojure]]
   [jonase/eastwood "0.1.4"]
   [lein-ancient "0.6.1"]
   [lein-cljfmt "0.1.4"]
   [lein-cloverage "1.0.2"]
   [lein-cprint "1.0.0"]
-  [lein-hiera "0.9.0"]
+  [lein-hiera "0.9.0" :exclusions [org.clojure/tools.namespace]]
   [lein-kibit "0.0.8"]
   [lein-marginalia "0.8.0"]
-  [lein-vanity "0.2.0"]]
+  [lein-vanity "0.2.0"]
+  #_ [mvxcvi/whidbey "1.0.0"]
+  ]
 
  :dependencies
  [[clj-stacktrace "0.2.7"]
@@ -23,7 +25,14 @@
   [org.clojure/tools.namespace "0.2.8"]
   [org.clojure/tools.trace "0.7.8"]]
 
+  :ultra {:repl         true
+          :stacktraces  true
+          :tests        true
+          :java         false}
+
+ #_
  :injections
+ #_
  [(let [pct-var (ns-resolve (doto 'clojure.stacktrace require) 'print-cause-trace)
         pst-var (ns-resolve (doto 'clj-stacktrace.repl require) 'pst+)]
     (alter-var-root pct-var (constantly (deref pst-var))))]
