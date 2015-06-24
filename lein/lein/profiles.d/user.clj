4f357a4c01be895a3af8252f334c085596e66330
Greg Look <greg@greg-look.net>
2015-06-23 23:21:15 -0700

    Update user profile.

--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -11,8 +11,8 @@
   [lein-ancient "0.6.1"]
   [lein-cljfmt "0.1.4"]
   [lein-cloverage "1.0.2"]
-  [lein-cprint "1.0.0"]
-  [lein-hiera "0.9.0" :exclusions [org.clojure/tools.namespace]]
+  [lein-cprint "1.1.0"]
+  [lein-hiera "0.9.0"]
   [lein-kibit "0.0.8"]
   [lein-marginalia "0.8.0"]
   [lein-vanity "0.2.0"]
@@ -21,13 +21,19 @@
  :dependencies
  [[clj-stacktrace "0.2.7"]
   [slamhound "1.5.5"]
-  [org.clojure/tools.namespace "0.2.8"]
+  [org.clojure/tools.namespace "0.2.10"]
   [org.clojure/tools.trace "0.7.8"]]
 
  :injections
  [(let [pct-var (ns-resolve (doto 'clojure.stacktrace require) 'print-cause-trace)
         pst-var (ns-resolve (doto 'clj-stacktrace.repl require) 'pst+)]
     (alter-var-root pct-var (constantly (deref pst-var))))]
+
+ :whidbey
+ {:width 200
+  :map-delimiter ","
+  :escape-types '#{clj_http.headers.HeaderMap}
+  }
 
  :codox
  {:defaults {:doc/format :markdown}
