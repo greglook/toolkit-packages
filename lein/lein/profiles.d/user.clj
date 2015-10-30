348b062fcb26ef4591eb382d7325372ba2aae849
Greg Look <greg@greg-look.net>
2015-10-29 18:59:11 -0700

    Upgrade lein user profile.

--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -1,28 +1,27 @@
 {:aliases
  {"doc-lit" ^:displace ["marg" "--dir" "doc/marginalia"]
-  "docs" ["do" ["doc"] ["doc-lit"] ["hiera"]]
+  "docs" ["do" ["codox"] ["doc-lit"] ["hiera"]]
   "tests" ["do" ["check"] ["test"] ["cloverage"]]
   "slamhound" ["run" "-m" "slam.hound"]}
 
  :plugins
- [[codox "0.8.10"]
-  [com.jakemccrary/lein-test-refresh "0.5.3" :exclusions [org.clojure/clojure]]
-  [jonase/eastwood "0.1.4"]
-  [lein-ancient "0.6.1"]
-  [lein-cljfmt "0.1.4"]
-  [lein-cloverage "1.0.2"]
+ [[com.jakemccrary/lein-test-refresh "0.11.0"
+   :exclusions [org.clojure/clojure]]
+  [lein-ancient "0.6.7"]
+  [lein-cloverage "1.0.6"]
+  [lein-codox "0.9.0"]
   [lein-cprint "1.1.0"]
   [lein-hiera "0.9.0"]
-  [lein-kibit "0.0.8"]
+  [lein-kibit "0.1.2"]
   [lein-marginalia "0.8.0"]
   [lein-vanity "0.2.0"]
-  [mvxcvi/whidbey "1.0.0"]]
+  [mvxcvi/whidbey "1.2.0"]]
 
  :dependencies
- [[clj-stacktrace "0.2.7"]
+ [[clj-stacktrace "0.2.8"]
   [slamhound "1.5.5"]
   [org.clojure/tools.namespace "0.2.10"]
-  [org.clojure/tools.trace "0.7.8"]]
+  [org.clojure/tools.trace "0.7.9"]]
 
  :injections
  [(let [pct-var (ns-resolve (doto 'clojure.stacktrace require) 'print-cause-trace)
@@ -31,8 +30,7 @@
 
  :whidbey
  {:width 200
-  :map-delimiter ","
-  :escape-types '#{clj_http.headers.HeaderMap}}
+  :map-delimiter ","}
 
  :codox
  {:defaults {:doc/format :markdown}
