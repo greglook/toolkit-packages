043ca8151744c237782e44852823860b9e372fd9
Greg Look <greg@greg-look.net>
2016-12-20 10:24:19 -0800

    Upgrade lein plugins in user profile.

--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -8,17 +8,17 @@
 
  :plugins
  [[org.clojure/clojure "1.8.0"]
-  [com.jakemccrary/lein-test-refresh "0.16.0"]
+  [com.jakemccrary/lein-test-refresh "0.18.1"]
   [mvxcvi/whidbey "1.3.1"]
   [lein-ancient "0.6.10"]
-  [lein-cljfmt "0.5.5"]
-  [lein-codox "0.9.5"]
+  [lein-cljfmt "0.5.6"]
+  [lein-codox "0.10.2"]
   [lein-cprint "1.2.0"]
   [lein-hiera "0.9.5"]
-  [lein-kibit "0.1.2"]
+  [lein-kibit "0.1.3"]
   [lein-vanity "0.2.0"]
   [michaelblume/lein-marginalia "0.9.0"]
-  [rfkm/lein-cloverage "1.0.8"]
+  [lein-cloverage "1.0.9"]
   [michaelblume/lein-marginalia "0.9.0"
    :exclusions [org.clojure/clojurescript]]]
 
