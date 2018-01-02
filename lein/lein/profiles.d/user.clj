277d5c70586f4de9546216a957966c6ceda68890
Greg Look <greg@greg-look.net>
2018-01-02 09:03:23 -0800

    Update lein user profile.

--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -6,18 +6,22 @@
  [[org.clojure/clojure "1.8.0"]
   [com.jakemccrary/lein-test-refresh "0.21.1"]
   [mvxcvi/whidbey "1.3.2"]
+  [mvxcvi/lein-cljfmt "0.7.0-SNAPSHOT"] ;[rewrite-cljs "0.4.3"]
   [lein-ancient "0.6.14"]
-  [lein-cljfmt "0.5.7"]
   [lein-codox "0.10.3"]
   [lein-cprint "1.3.0"]
   [lein-hiera "1.0.0"]
-  [lein-kibit "0.1.5"]
+  [lein-kibit "0.1.6"]
   [lein-vanity "0.2.0"]
   [lein-cloverage "1.0.10"]
+  [lein-eftest "0.4.1"]
   [michaelblume/lein-marginalia "0.9.0"
    :exclusions [org.clojure/clojurescript]]
   [venantius/yagni "0.1.4"]
-  [walmartlabs/vizdeps "0.1.6"]]
+  [walmartlabs/vizdeps "0.1.6"]
+
+  ; Version conflict resolution
+  [org.clojure/tools.reader "1.0.2"]]
 
  :dependencies
  [[clj-stacktrace "0.2.8"]
