fd2669fb5c4f062f265f432ce0e0bd493816910d
Greg Look <greg@mvxcvi.com>
2020-01-06 15:14:18 -0800

    Update leiningen configs.

--- lein/lein/profiles.d/user.clj
+++ lein/lein/profiles.d/user.clj
@@ -1,33 +1,26 @@
-{:aliases
- {"doc-lit" ^:displace ["marg" "--dir" "target/doc/marginalia"]
-  "coverage" ^:displace ["cloverage"]}
+{:signing
+ {:gpg-key "greg@mvxcvi.com"}
 
  :plugins
- [[org.clojure/clojure "1.9.0"]
-  [org.clojure/tools.namespace "0.3.0-alpha4"]
-  [org.clojure/tools.reader "1.0.2"]
-  [com.jakemccrary/lein-test-refresh "0.22.0"]
-  [mvxcvi/whidbey "1.3.2"]
+ [[org.clojure/clojure "1.10.1"]
+  [mvxcvi/whidbey "2.2.0"]
   [lein-ancient "0.6.15"]
-  [mvxcvi/lein-cljfmt "0.7.0-SNAPSHOT"]
-  [lein-codox "0.10.3"]
-  [lein-cprint "1.3.0"]
-  [lein-hiera "1.0.0"]
-  [lein-kibit "0.1.6"]
+  [lein-codox "0.10.7"]
+  [lein-cprint "1.3.2"]
+  [lein-hiera "1.1.0"]
+  ;[lein-nvd "1.1.1" :exclusions [org.slf4j/slf4j-api org.slf4j/jcl-over-slf4j]]
   [lein-vanity "0.2.0"]
-  [lein-cloverage "1.0.10"]
-  [lein-eftest "0.4.1"]
-  [lein-marginalia "0.9.1"]
-  [venantius/yagni "0.1.4"]
-  [walmartlabs/vizdeps "0.1.6"]
-
-  ; Version conflict resolution
-  [org.clojure/tools.reader "1.0.2"]]
+  [lein-cloverage "1.1.2"]
+  [lein-collisions "0.1.4"]
+  ;[lein-marginalia "0.9.1"]
+  [lein-shell "0.5.0"]
+  [com.jakemccrary/lein-test-refresh "0.24.1"]
+  ;[venantius/yagni "0.1.7" :exclusions [org.clojure/tools.logging]]
+  ,,,]
 
  :dependencies
  [[clj-stacktrace "0.2.8"]
-  [org.clojure/tools.namespace "0.2.11"]
-  [pjstadig/humane-test-output "0.8.3"]]
+  [pjstadig/humane-test-output "0.9.0"]]
 
  :injections
  [(let [pct-var (ns-resolve (doto 'clojure.stacktrace require) 'print-cause-trace)
@@ -36,12 +29,18 @@
   (require 'pjstadig.humane-test-output)
   (pjstadig.humane-test-output/activate!)]
 
+ :middleware
+ [whidbey.plugin/repl-pprint]
+
  :whidbey
  {:width 150
   :map-delimiter ","
+  :namespace-maps true
   :color-scheme {:nil [:blue]}
   :tag-types {java.lang.Class {'jvm/class #(symbol (.getName %))}
-              java.time.Instant {'inst str}}}
+              java.time.Instant {'inst str}
+              'org.joda.time.DateTime {'joda/inst str}
+              'org.joda.time.UTCDateTimeZone {'joda/zone str}}}
 
  :hiera
  ^:displace
