e407c9b5ad8e26d036097269247ab4d1fe9efaf2
Greg Look <greg@mvxcvi.com>
2026-01-06 00:12:15 -0800

    Add XDG env vars for fish, update configs.

---
+++ fish/dot-config/fish/conf.d/xdg.fish
@@ -0,0 +1,28 @@
+# Set XDG environment variables and tool-specific configuration to keep the
+# home directory clean(er).
+# https://wiki.archlinux.org/title/XDG_Base_Directory
+
+# Set base XDG vars
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_STATE_HOME $HOME/.local/state
+set -gx XDG_DATA_HOME $HOME/.local/share
+
+# AWS CLI
+set -gx AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
+set -gx AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
+
+# GPG
+set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
+
+# Leiningen
+set -gx LEIN_HOME $XDG_DATA_HOME/lein
+
+# NPM
+set -gx NPM_CONFIG_INIT_MODULE  $XDG_CONFIG_HOME/npm/config/npm-init.js
+set -gx NPM_CONFIG_CACHE $XDG_CACHE_HOME/npm
+set -gx NPM_CONFIG_TMP $XDG_RUNTIME_DIR/npm
+#set -gx NPM_CONFIG_PREFIX $HOME/.local/
+
+# wget
+alias wget="wget --hsts-file=$XDG_DATA_HOME/wget-hsts"
