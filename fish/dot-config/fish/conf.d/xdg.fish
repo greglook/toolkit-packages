6e93580fbe58a072e4d86ee5d0fdc7c8c3488b54
Greg Look <greg@mvxcvi.com>
2026-01-06 11:33:07 -0800

    Add XDG configs for Clojure gitlibs and Sqlite history.

--- fish/dot-config/fish/conf.d/xdg.fish
+++ fish/dot-config/fish/conf.d/xdg.fish
@@ -12,6 +12,9 @@ set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
 set -gx AWS_SHARED_CREDENTIALS_FILE $XDG_CONFIG_HOME/aws/credentials
 
+# Clojure
+set -gx GITLIBS $XDG_STATE_HOME/clojure/gitlibs
+
 # GPG
 set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
 
@@ -23,6 +26,9 @@ set -gx NPM_CONFIG_INIT_MODULE  $XDG_CONFIG_HOME/npm/config/npm-init.js
 set -gx NPM_CONFIG_CACHE $XDG_CACHE_HOME/npm
 set -gx NPM_CONFIG_TMP $XDG_RUNTIME_DIR/npm
 #set -gx NPM_CONFIG_PREFIX $HOME/.local/
+
+# Sqlite
+set -gx SQLITE_HISTORY $XDG_CACHE_HOME/sqlite_history
 
 # wget
 alias wget="wget --hsts-file=$XDG_DATA_HOME/wget-hsts"
